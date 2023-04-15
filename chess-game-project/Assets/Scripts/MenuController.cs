81ff712b71298c5143235a34456f56c907ef197a
Johann Daflon <johanndaflon@id.uff.br>
2023-04-15 17:41:01 -0300

    Menu Simples Adicionado
    Menu funcional, porém simples.

---
+++ chess-game-project/Assets/Scripts/MenuController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public string mainSceneName = "Game";
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(mainSceneName);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
